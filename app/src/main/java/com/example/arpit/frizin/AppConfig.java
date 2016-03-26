c29dd36c5ad86d5056bf1aefee53fe652bb57976
Arpit Singh <arpitsinghnitd@gmail.com>
2016-03-26 20:01:21 +0530

    User Login and Register Code added

---
+++ app/src/main/java/com/example/arpit/frizin/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.arpit.frizin;
+
+/**
+ * Created by arpit on 26/3/16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.16.41.13/frizin/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.16.41.13/frizin/register.php";
+}
